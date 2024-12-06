c7c8c21d71c1973c36dbb337210a3ddd423dc463
Gonzalo A. Arenas Flores <gonzalo.arenas.flores@gmail.com>
2024-12-06 16:40:35 -0300

    v1.0.1
    - Players per team feature moved to the next version.
    - Teams organization functionality.

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Simple tool to enter a players list and organize teams randomly
 ## MVP version includes
 - Players list, including their positions.
 - Number of teams to organize.
-- Number of players per team.
 
 ## Next version
+- Number of players per team.
 - Match features: 
 - Scores
 - Live playing time 
--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const settings = {
 
 const outcome = {
   teams: [],
-  bench: [],
+  bench: {
+    pointGuards: [],
+    shootingGuards: [],
+    smallForwards: [],
+    powerForwards: [],
+    centers: [],
+  },
 };
 
 function switchUI() {
@@ -35,7 +41,6 @@ function getFormData() {
     players.centers = getPlayersFromForm("centerPlayersList");
 
     settings.teamsNumber = parseInt(document.getElementById("teamsNumber").value);
-    settings.playersPerTeam = parseInt(document.getElementById("playerPerTeam").value);
 
     return true;
   } catch (error) {
@@ -48,11 +53,21 @@ function validateData() {
   const errorsList = [];
 
   try {
-    players.pointGuards.length == 0 ? errorsList.push("- Point guards missings") : null;
-    players.shootingGuards.length == 0 ? errorsList.push("- Shooting guards missings") : null;
-    players.smallForwards.length == 0 ? errorsList.push("- Small forwards missings") : null;
-    players.powerForwards.length == 0 ? errorsList.push("- Power forwards missings") : null;
-    players.centers.length == 0 ? errorsList.push("- Centers missings") : null;
+    players.pointGuards.length < settings.teamsNumber
+      ? errorsList.push("- There is no enough point guards for every team")
+      : null;
+    players.shootingGuards.length < settings.teamsNumber
+      ? errorsList.push("- There is no enough shooting guards for every team")
+      : null;
+    players.smallForwards.length < settings.teamsNumber
+      ? errorsList.push("- There is no enough small forwards for every team")
+      : null;
+    players.powerForwards.length < settings.teamsNumber
+      ? errorsList.push("- There is no enough power forwards for every team")
+      : null;
+    players.centers.length < settings.teamsNumber
+      ? errorsList.push("- There is no enough centers for every team")
+      : null;
 
     if (errorsList.length > 0) {
       throw "Missings data";
@@ -67,16 +82,79 @@ function validateData() {
   }
 }
 
+function getRandomPlayer(position) {
+  const randomIndex = Math.floor(Math.random() * players[position].length);
+  return players[position].pop(randomIndex);
+}
+
+function organizeTeams() {
+  outcome.teams = [];
+  for (let teamNumber = 0; teamNumber < settings.teamsNumber; teamNumber++) {
+    outcome.teams.push({
+      pointGuard: getRandomPlayer("pointGuards"),
+      shootingGuard: getRandomPlayer("shootingGuards"),
+      smallForward: getRandomPlayer("smallForwards"),
+      powerForward: getRandomPlayer("powerForwards"),
+      center: getRandomPlayer("centers"),
+    });
+  }
+
+  outcome.bench.pointGuards = players.pointGuards;
+  outcome.bench.shootingGuards = players.shootingGuards;
+  outcome.bench.smallForwards = players.smallForwards;
+  outcome.bench.powerForwards = players.powerForwards;
+  outcome.bench.centers = players.centers;
+}
+
+function showOutcome() {
+  const teamTmp = `<article class="team">
+    <h3>Team {teamNumber}</h3>
+    <ul>
+      <li><strong>Point guard:</strong> {pointGuard}</li>
+      <li><strong>Shooting guard:</strong> {shootingGuard}</li>
+      <li><strong>Small forward:</strong> {smallForward}</li>
+      <li><strong>Power forward:</strong> {powerForward}</li>
+      <li><strong>Center:</strong> {center}</li>
+    </ul>
+  </article>`;
+
+  const renderTeams = [];
+  outcome.teams.forEach((team, index) => {
+    console.log(team);
+    renderTeams.push(
+      teamTmp
+        .replace("{teamNumber}", index + 1)
+        .replace("{pointGuard}", team.pointGuard)
+        .replace("{shootingGuard}", team.shootingGuard)
+        .replace("{smallForward}", team.smallForward)
+        .replace("{powerForward}", team.powerForward)
+        .replace("{center}", team.center)
+    );
+  });
+  document.querySelector("#outcome div.teams").innerHTML = renderTeams.join('');
+
+  const renderBench = [];
+  renderBench.push("<h2>Bench</h2>");
+  renderBench.push("<h3>Point guards</h3>");
+  renderBench.push(`<ul><li>${outcome.bench.pointGuards.join('</li><li>')}</li></ul>`);
+  renderBench.push("<h3>Shooting guards</h3>");
+  renderBench.push(`<ul><li>${outcome.bench.shootingGuards.join('</li><li>')}</li></ul>`);
+  renderBench.push("<h3>Small forwards</h3>");
+  renderBench.push(`<ul><li>${outcome.bench.smallForwards.join('</li><li>')}</li></ul>`);
+  renderBench.push("<h3>Power forwards</h3>");
+  renderBench.push(`<ul><li>${outcome.bench.powerForwards.join('</li><li>')}</li></ul>`);
+  renderBench.push("<h3>Centers</h3>");
+  renderBench.push(`<ul><li>${outcome.bench.centers.join('</li><li>')}</li></ul>`);
+  document.querySelector("#outcome div.bench").innerHTML = renderBench.join('');
+}
+
 function createTeams() {
   const isDataObtained = getFormData();
   const isDataValid = isDataObtained ? validateData() : false;
 
   if (isDataObtained && isDataValid) {
-    console.log(players);
-    console.log(settings);
-
-
-
+    organizeTeams();
+    showOutcome();
     switchUI();
   }
 }
